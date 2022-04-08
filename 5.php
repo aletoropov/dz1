cc4745b2f44896065fed25e6d912cd03a652e807
Alexandr Toropov <toropoviste@yandex.ru>
2022-04-08 16:03:34 +0300

    задание №5 -объединение массивов и вывод

--- 5.php
+++ 5.php
@@ -21,3 +21,35 @@ $opel = [
     'yaer'   => '2010',
 ];
 
+$cars = [
+    'bmw' => [
+        'model'  => 'X5',
+        'speed'  => 120,
+        'doors'  => 5,
+        'yaer'   => '2015',
+    ],
+
+    'toyota' => [
+        'model'  => 'Corolla',
+        'speed'  => 140,
+        'doors'  => 4,
+        'yaer'   => '2020',
+    ],
+
+    'opel' => [
+        'model'  => 'Astra',
+        'speed'  => 180,
+        'doors'  => 3,
+        'yaer'   => '2010',
+    ],
+];
+
+foreach ($cars as $model => $brand) {
+    echo 'CAR ' . $model . '<br>';
+    if (is_array($brand)) {
+        foreach ($brand as $key) {
+            echo $key . ' ';
+        }
+    }
+    echo '<br>';
+}
