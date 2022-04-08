7bd763b7d6e7cedb093a68cbec04ec54c26fc48e
Alexandr Toropov <toropoviste@yandex.ru>
2022-04-08 15:50:11 +0300

    задание №5 - создал массивы toyota и opel

--- 5.php
+++ 5.php
@@ -7,4 +7,17 @@ $bmw = [
     'yaer'   => '2015',
 ];
 
+$toyote = [
+    'model'  => 'Corolla',
+    'speed'  => 140,
+    'doors'  => 4,
+    'yaer'   => '2020',
+];
+
+$opel = [
+    'model'  => 'Astra',
+    'speed'  => 180,
+    'doors'  => 3,
+    'yaer'   => '2010',
+];
 
